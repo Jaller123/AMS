802dc41e7aa07665f339ecdd1c8ef6473c2251ea
Kenatho <89846381+Kenatho@users.noreply.github.com>
2024-12-09 15:39:42 +0100

    Lagt in inputfält i Res

--- ams/src/components/ResForm.jsx
+++ ams/src/components/ResForm.jsx
@@ -7,15 +7,36 @@ const ResForm = () => {
       <h2>Response</h2>
       <div className={styles.formGroup}>
         <label htmlFor="reqUrl">Status Code:</label>
-       
+        <textarea
+          id="reqBody"
+          name="reqBody"
+          rows="1"
+          placeholder='200, OK '
+          className={styles.textarea}
+        ></textarea>
       </div>
       <div className={styles.formGroup}>
       </div>
       <div className={styles.formGroup}>
         <label htmlFor="reqHeaders">Headers:</label>
+        <textarea
+          id="reqBody"
+          name="reqBody"
+          rows="1"
+          placeholder='{"Content-Type": "application/json"}'
+          className={styles.textarea}
+        ></textarea>
       </div>
       <div className={styles.formGroup}>
         <label htmlFor="reqBody">Body:</label>
+        <textarea
+          id="reqBody"
+          name="reqBody"
+          rows="6"
+          placeholder='"message": "Request successful",
+        '
+          className={styles.textarea}
+        ></textarea>
       </div>
     </section>
   )
