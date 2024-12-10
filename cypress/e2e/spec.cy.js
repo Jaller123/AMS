0aa92caf66109800c91e653bc97548ec40663c6d
youal <youalwol@yahoo.com>
2024-12-10 09:37:35 +0100

    kört lite tester

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,15 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe('ReqForm Component Tests', () => {
+
+  it('renders the form correctly', () => {
+
+    cy.visit('http://localhost:5176/'); 
+   
+    cy.get('h2').should('contain', 'Request');
+
+    cy.get('[ data-test-id="input-url"]').type("url")
+    cy.get('[ data-test-id="input-field"]').type("url")
+    
+  });
+
+
+});
