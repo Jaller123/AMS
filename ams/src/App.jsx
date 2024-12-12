2d0e7fd1f2f7087def5c6f933e41d9420f27dc03
youal <youalwol@yahoo.com>
2024-12-12 10:39:48 +0100

    Lagt till funktionalitet för att skicka HTTP-förfrågningar

--- ams/src/App.jsx
+++ ams/src/App.jsx
@@ -1,18 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import ReqForm from "./components/ReqForm";
-
-import Button from "./components/button";
-
 import ResForm from "./components/ResForm";
 
 const App = () => {
+  const [response, setResponse] = useState({});
+
+  const handleRequestData = async (data) => {
+    try {
+      const payload = {
+        request: {
+          method: data.method,
+          url: data.url
+        },
+        response: {
+          status: 200,
+          headers: data.headers ? data.headers : {},
+          
+          body: data.body ? JSON.stringify(data.body) : ""
+        }
+      };
+
+      const res = await fetch("http://localhost:8088/__admin/mappings", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(payload)
+      });
+
+      if (res.ok) {
+        const responseBody = await res.text();
+        console.log(responseBody);
+        setResponse({
+          status: res.status,
+          body: responseBody
+        });
+      } else {
+        console.error("WireMock responded with error status:", res.status);
+        const errorBody = await res.text();
+        console.error("Error details:", errorBody);
+      }
+    } catch (error) {
+      console.error("Request failed!", error);
+    }
+  };
+
   return (
     <div>
-      <ReqForm />
-      <Button />
-      <ResForm />
+      <ReqForm onRequestChange={handleRequestData} />
+      <ResForm response={response} />
     </div>
   );
 };
 
 export default App;
+
