8a0a09d0a9cdd504771225064cba2fad7fdbe3a1
youal <youalwol@yahoo.com>
2024-12-13 16:46:24 +0100

    ändrade till föregånde kod

--- ams/src/components/button.jsx
+++ ams/src/components/button.jsx
@@ -1,10 +1,6 @@
 import React from "react";
 import styles from "./Button.module.css";
 
-const Button = ({ onClick, label }) => {
-  return (
-    <button className={styles.button} onClick={onClick}>
-      {label}
 const Button = ({ url, method, headers, body, onRequestChange }) => {
   
   const handleSubmit = () => {
