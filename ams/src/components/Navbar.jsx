353020b33db7c183c872205de4d9e0debce2d5ab
Feridali <ferid62@hotmail.com>
2025-01-27 15:12:44 +0100

    första traffic sidan

--- ams/src/components/Navbar.jsx
+++ ams/src/components/Navbar.jsx
@@ -6,13 +6,27 @@ import logo from "../assets/AMSlogo.png";
 const Navbar = () => {
   return (
     <nav className={styles.navbar}>
-      <div className={styles.navbarBrand}><img src={logo} alt="AMS" className={styles.logo} /></div>
+      <div className={styles.navbarBrand}>
+        <img src={logo} alt="AMS" className={styles.logo} />
+      </div>
       <ul className={styles.navbarMenu}>
         <li className={styles.menuItem}>
-          <Link to="/" className={styles.link}>Home</Link> {/* Länk till startsidan */}
+          <Link to="/" className={styles.link}>
+            Home
+          </Link>{" "}
+          {/* Länk till startsidan */}
         </li>
         <li className={styles.menuItem}>
-          <Link to="/mappings" className={styles.link}>Mappings</Link> {/* Länk till mappningar */}
+          <Link to="/mappings" className={styles.link}>
+            Mappings
+          </Link>{" "}
+          {/* Länk till mappningar */}
+        </li>
+        <li className={styles.menuItem}>
+          <Link to="/traffic" className={styles.link}>
+            Traffic
+          </Link>{" "}
+          {/* New link */}
         </li>
       </ul>
     </nav>
